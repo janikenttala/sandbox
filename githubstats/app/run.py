87d8a4b7e9f78be675876fadd21fa5850b3818a9
Jani Kenttälä <janike@iki.fi>
2017-07-03 00:00:54 +0300

    A server to collect and parse github data.

---
+++ githubstats/app/run.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3
+"""Serve github timeline """
+# based on example at:
+# https://daanlenaerts.com/blog/2015/06/03/create-a-simple-http-server-with-python-3/
+
+from http.server import BaseHTTPRequestHandler, SimpleHTTPRequestHandler, HTTPServer
+import urllib.request
+import time
+import json
+
+import timelineparse
+# HTTPRequestHandler class
+
+lastCache = 0
+raw_timeline = ""
+
+class timelineServer(SimpleHTTPRequestHandler):
+    """blah"""
+    def __init__(self,request, client_address, server):
+        self.raw_timeline = ""
+        SimpleHTTPRequestHandler.__init__(self,request, client_address, server)
+
+    def err(self):
+        self.send_response(404)
+        self.send_header('Content-type', "text/html")
+        self.end_headers()
+        message = "ERRR" + self.path
+        self.wfile.write(bytes(message, "utf8"))
+
+    def _send(self, message, content_type):
+        """ blah """
+        self.send_response(200)
+        self.send_header('Content-type', content_type)
+        self.end_headers()
+        self.wfile.write(bytes(message + "\n", "utf8"))
+        return
+
+    def do_GET(self):
+        # Investigate: initialising self.raw_timeline to "" and assigning 
+        # value to it is not preserved, next do_GET it is empty again.
+        global raw_timeline
+
+        allowed = ["/timeline.html", "/timeline.css", "/timeline.js"]
+        #allowed.append("/data/timeline.json") # test data
+
+        if self.path in allowed:
+            SimpleHTTPRequestHandler.do_GET(self)
+        elif self.path == "/data/timeline.json":
+            timeline = list()
+            if cache_expired(10):
+                timeline = list()
+                raw_timeline = getgit("evilon",
+                                      "<key>",
+                                      debug=True)
+            for line in  parsetimeline(raw_timeline):
+                timeline.append(line)
+
+            self._send(json.dumps(timeline), "application/json")
+        else:
+            self.err()
+
+
+def run():
+    """ Start the webserver """
+    print('starting server...')
+    server_address = ('127.0.0.1', 8000)
+    httpd = HTTPServer(server_address, timelineServer)
+    print('running server...')
+    httpd.serve_forever()
+
+def cache_expired(cache_time):
+    """ Retrieve only if cache_time has passed """
+    global lastCache
+    now = time.time()
+    if now - lastCache > cache_time:
+        lastCache = now
+        return True
+    else:
+        return False
+
+def parsetimeline(timeline):
+    timeline = timeline.encode('utf-8')
+    for line in timelineparse.parse(timeline):
+        if len(line) == 0: continue
+        yield json.loads(line)
+
+
+def getgit(user, key, debug=False):
+    """ Retrieve git timeline """
+    if debug:
+        debug_json = open("./data/1.json", "r").read()
+        return debug_json
+
+    url = "https://api.github.com/users/%s/events?page=$i&per_page=100" % (user)
+
+    with urllib.request.urlopen(url) as handle:
+        parsed = parsetimeline(handle.read().decode('utf-8'))
+    return parsed
+
+run()
---
+++ githubstats/app/timelineparse.py
@@ -0,0 +1,104 @@
+import subprocess
+import sys,time
+
+def process(raw, jq):
+    cmd = subprocess.Popen(jq,
+                           stdout=subprocess.PIPE,
+                           stderr=subprocess.STDOUT,
+                           stdin=subprocess.PIPE,
+                           shell=False,
+                           bufsize=0)
+
+    stdout, stderr = cmd.communicate(raw)
+    for line in stdout.decode('utf-8').split("\n"):
+        yield line
+
+def issues(raw):
+    jq = ['jq', '-c',
+          '[ .[] | select( .type | contains("IssuesEvent"))] | .[] | { \
+           "repo": .repo.name, \
+           "action": .payload.action, \
+           "time": .created_at, \
+           "target": .payload.issue.title, \
+           "message": .payload.issue.title \
+           }']
+
+    for line in process(raw, jq):
+        yield line
+
+def issuecomments(raw):
+    jq = ['jq', '-c', 
+          '[ .[] | select( .type | contains("IssueCommentEvent"))] | .[] | { \
+          "repo": .repo.name, \
+          "action": "issue comment", \
+          "time": .created_at, \
+          "target": .payload.issue.title, \
+          "message": .payload.comment.body \
+          }']
+
+    for line in process(raw, jq):
+        yield line
+
+def pullrequest(raw):
+    jq = ['jq', '-c', '[ .[] | select( .type | contains("PullRequestEvent"))] | .[] | { \
+        "time": .created_at, \
+        "repo": .repo.name, \
+        "action": .payload.action, \
+        "target": .payload.pull_request.title, \
+        "message": .payload.pull_request.body \
+        }']
+
+    for line in process(raw, jq):
+        yield line
+
+def push(raw):
+    jq = ['jq', '-c', '[ .[] | select( .type | contains("PushEvent"))] | .[] | { \
+        "time": .created_at, \
+        "repo": .repo.name, \
+        "action": "push", \
+        "target": "commit", \
+        "message": .payload.commits[].message \
+        }']
+
+    for line in process(raw, jq):
+        yield line
+
+def create(raw):
+    jq = ['jq', '-c', '[ .[] | select( .type | contains("CreateEvent"))] | .[] | { \
+        "time": .created_at, \
+        "repo": .repo.name, \
+        "action": "create", \
+        "target": .payload.ref_type, \
+        "message": .payload.ref \
+        }']
+
+    for line in process(raw, jq):
+        yield line
+
+def watch(raw):
+    jq = ['jq', '-c', '[ .[] | select( .type | contains("WatchEvent"))] | .[] | { \
+        "time": .created_at, \
+        "repo": .repo.name, \
+        "action": "watch", \
+        "target": "timeline", \
+        "message": .payload.action \
+        }']
+    
+    for line in process(raw, jq):
+        yield line
+    
+def parse(data):
+    for line in issues(data):
+        yield line
+
+    for line in issuecomments(data):
+        yield line
+
+    for line in push(data):
+        yield line
+
+    for line in create(data):
+        print(line)
+        yield line
+
+
